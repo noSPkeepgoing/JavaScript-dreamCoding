887a1a686de006b646600e2fea50f8b19ac3f92d
sunpaaaa <sunpaaaa@gmail.com>
2023-04-11 17:13:56 +0900

    built in functions and library in javascript

---
+++ 7.built-in/5.math.js
@@ -0,0 +1,21 @@
+// Math
+// static properties, method
+console.log(Math.E); // 오일러의 상수, 자연로그의 밑
+console.log(Math.PI); // 원주율 PI값
+
+// static method
+console.log(Math.abs(-10)); // 절대값
+console.log(Math.ceil(1.4)); // 소수점 이하를 올림
+console.log(Math.floor(1.4)); // 소수점 이하를 내림
+console.log(Math.round(1.4)); // 소수점 이하를 반올림
+console.log(Math.trunc(1.4321)); // 정수만 반환
+
+console.log(Math.max(1, 3, 4, 5)); // 최대값
+console.log(Math.min(1, 3, 4, 5)); // 최소값
+
+console.log(3 ** 2);
+console.log(Math.pow(3, 2)); // 거듭제곱
+console.log(Math.sqrt(9)); // 제곱근
+
+console.log(Math.random()); // 0~1 사이 랜던 값 반환
+console.log(Math.floor(Math.random() * 10 + 1)); // 1~10 사이 랜던 값 반환
